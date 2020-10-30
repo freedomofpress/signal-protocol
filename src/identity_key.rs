56ad8a54290cac6573348735d11fa95d84e11bb3
redshiftzero <jen@freedom.press>
2020-10-30 15:06:47 -0400

    feat: finish identity_key.rs porting

--- src/identity_key.rs
+++ src/identity_key.rs
@@ -16,11 +16,16 @@ pub struct IdentityKey {
 
 #[pymethods]
 impl IdentityKey {
+    // The behavior of libsignal_protocol_rust::IdentityKey::decode is provided
+    // by the new() function.
     #[new]
-    pub fn new(public_key: &[u8]) -> Self {
-        Self { key: libsignal_protocol_rust::IdentityKey::try_from(public_key).unwrap() }
+    pub fn new(public_key: &[u8]) -> PyResult<Self> {
+        Ok(Self { key: libsignal_protocol_rust::IdentityKey::try_from(public_key).unwrap() })
     }
 
+    // There is no libsignal_protocol_rust::IdentityKey::public_key method,
+    // instead one can use the serialized public key via this serialize()
+    // method.
     pub fn serialize(&self, py: Python) -> PyResult<PyObject> {
         Ok(PyBytes::new(py, &self.key.serialize()).into())
     }
@@ -31,8 +36,18 @@ pub struct IdentityKeyPair {
     pub key: libsignal_protocol_rust::IdentityKeyPair,
 }
 
+/// ## Note on comparison with upstream crate:
+///
+/// There is no identity_key method exposed, but one can extract the public
+/// key and private key bytes via the public_key() and public_key() methods
+/// respectively, or the serialized identity key pair via serialize().
 #[pymethods]
 impl IdentityKeyPair {
+    #[new]
+    pub fn new(identity_key_pair_bytes: &[u8]) -> PyResult<Self> {
+        Ok( Self{ key: libsignal_protocol_rust::IdentityKeyPair::try_from(identity_key_pair_bytes).unwrap() } )
+    }
+
     #[staticmethod]
     pub fn generate() -> PyResult<Self> {
         let mut csprng = OsRng;
@@ -42,6 +57,14 @@ impl IdentityKeyPair {
 
     pub fn public_key(&self, py: Python) -> PyResult<PyObject> {
         Ok(PyBytes::new(py, &self.key.public_key().serialize()).into())
+    }
+
+    pub fn private_key(&self, py: Python) -> PyResult<PyObject> {
+        Ok(PyBytes::new(py, &self.key.private_key().serialize()).into())
+    }
+
+    pub fn serialize(&self, py: Python) -> PyResult<PyObject> {
+        Ok(PyBytes::new(py, &self.key.serialize()).into())
     }
 }
 
--- tests/test_identity_key.py
+++ tests/test_identity_key.py
@@ -1,12 +1,23 @@
 from signal_protocol import identity_key
 
 
-def test_identity_key_init_and_serialization():
+def test_identity_key_pair_init_and_serialization():
     identity_keypair = identity_key.IdentityKeyPair.generate()
-    alice_public = identity_keypair.public_key()
-    alice_identity_key = identity_key.IdentityKey(alice_public)
-    assert alice_public == alice_identity_key.serialize()
+    alice_public_serialized = identity_keypair.public_key()
+    alice_identity_key = identity_key.IdentityKey(alice_public_serialized)
+    assert alice_public_serialized == alice_identity_key.serialize()
 
 
-def test_identity_key_generate():
+def test_identity_key_pair_generate():
     alice_identity_key = identity_key.IdentityKeyPair.generate()
+    assert alice_identity_key.public_key()
+    assert alice_identity_key.private_key()
+
+
+def test_identity_key_pair_from_bytes():
+    test_keypair = identity_key.IdentityKeyPair.generate()
+    test_keypair_serialized = test_keypair.serialize()
+
+    decoded_keypair = identity_key.IdentityKeyPair(test_keypair_serialized)
+    assert decoded_keypair.public_key() == test_keypair.public_key()
+    assert decoded_keypair.private_key() == test_keypair.private_key()
