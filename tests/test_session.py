01acdd06648fb02b5b63c13eec40b6cfa26e8ef8
redshiftzero <jen@freedom.press>
2020-11-06 15:23:07 -0500

    test(session): repeat bundle

--- tests/test_session.py
+++ tests/test_session.py
@@ -237,3 +237,88 @@ def test_bad_signed_pre_key_signature():
             )
 
     session.process_prekey_bundle(bob_address, alice_store, bob_pre_key_bundle)
+
+
+def test_repeat_bundle_message_v3():
+    alice_address = address.ProtocolAddress("+14151111111", DEVICE_ID)
+    bob_address = address.ProtocolAddress("+14151111112", DEVICE_ID)
+
+    alice_identity_key_pair = identity_key.IdentityKeyPair.generate()
+    bob_identity_key_pair = identity_key.IdentityKeyPair.generate()
+
+    alice_registration_id = 1 #TODO: generate these
+    bob_registration_id = 2
+
+    alice_store = storage.InMemSignalProtocolStore(alice_identity_key_pair, alice_registration_id)
+    bob_store = storage.InMemSignalProtocolStore(bob_identity_key_pair, bob_registration_id)
+
+    bob_pre_key_pair = curve.KeyPair.generate()
+    bob_signed_pre_key_pair = curve.KeyPair.generate()
+
+    bob_signed_pre_key_public = bob_signed_pre_key_pair.public_key().serialize()
+
+    bob_signed_pre_key_signature = bob_store.get_identity_key_pair() \
+                                            .private_key() \
+                                            .calculate_signature(bob_signed_pre_key_public)
+
+    pre_key_id = 31337
+    signed_pre_key_id = 22
+
+    bob_pre_key_bundle = state.PreKeyBundle(
+        bob_store.get_local_registration_id(),
+        DEVICE_ID,
+        pre_key_id,
+        bob_pre_key_pair.public_key(),
+        signed_pre_key_id,
+        bob_signed_pre_key_pair.public_key(),
+        bob_signed_pre_key_signature,
+        bob_store.get_identity_key_pair().identity_key(),
+    )
+
+    session.process_prekey_bundle(
+        bob_address,
+        alice_store,
+        bob_pre_key_bundle,
+    )
+
+    assert alice_store.load_session(bob_address)
+    assert alice_store.load_session(bob_address).session_version() == 3
+
+    original_message = "Hobgoblins hold themselves to high standards of military honor"
+
+    outgoing_message1 = session_cipher.message_encrypt(alice_store, bob_address, original_message)
+    outgoing_message2 = session_cipher.message_encrypt(alice_store, bob_address, original_message)
+    outgoing_message1.message_type() == 3  # 3 == CiphertextMessageType::PreKey
+    outgoing_message2.message_type() == 3  # 3 == CiphertextMessageType::PreKey
+
+    incoming_message = protocol.PreKeySignalMessage.try_from(outgoing_message1.serialize())
+
+    bob_prekey = state.PreKeyRecord(pre_key_id, bob_pre_key_pair)
+    bob_store.save_pre_key(pre_key_id, bob_prekey)
+
+    signed_prekey = state.SignedPreKeyRecord(
+            signed_pre_key_id,
+            42,
+            bob_signed_pre_key_pair,
+            bob_signed_pre_key_signature,
+        )
+    bob_store.save_signed_pre_key(signed_pre_key_id, signed_prekey)
+
+    ptext = session_cipher.message_decrypt(bob_store, alice_address, incoming_message)
+    assert original_message == ptext.decode('utf8')
+
+    bob_outgoing = session_cipher.message_encrypt(bob_store, alice_address, original_message)
+    assert bob_outgoing.message_type() == 2  # 2 == CiphertextMessageType::Whisper
+
+    alice_decrypts = session_cipher.message_decrypt(alice_store, bob_address, bob_outgoing)
+    assert alice_decrypts.decode('utf8') == original_message
+
+    # Verify the second message can be processed
+
+    incoming_message2 = protocol.PreKeySignalMessage.try_from(outgoing_message2.serialize())
+    ptext = session_cipher.message_decrypt(bob_store, alice_address, incoming_message2)
+    assert original_message == ptext.decode('utf8')
+
+    bob_outgoing = session_cipher.message_encrypt(bob_store, alice_address, original_message)
+    alice_decrypts = session_cipher.message_decrypt(alice_store, bob_address, bob_outgoing)
+    assert alice_decrypts.decode('utf8') == original_message
