d8bd4d41c63a18feb51cf764f11d3682d5429518
redshiftzero <jen@freedom.press>
2020-11-06 15:56:14 -0500

    test(session): bad message bundles

--- tests/test_session.py
+++ tests/test_session.py
@@ -199,7 +199,7 @@ def test_bad_signed_pre_key_signature():
     signed_pre_key_id = 22
 
     for bit in range(8):
-        bit += len(bob_signed_pre_key_signature)
+        bit *= len(bob_signed_pre_key_signature)
 
         edit_point = bit // 8
         bad_signature = bob_signed_pre_key_signature[:edit_point] + \
@@ -322,3 +322,92 @@ def test_repeat_bundle_message_v3():
     bob_outgoing = session_cipher.message_encrypt(bob_store, alice_address, original_message)
     alice_decrypts = session_cipher.message_decrypt(alice_store, bob_address, bob_outgoing)
     assert alice_decrypts.decode('utf8') == original_message
+
+
+def test_bad_message_bundle():
+    alice_address = address.ProtocolAddress("+14151111111", DEVICE_ID)
+    bob_address = address.ProtocolAddress("+14151111112", DEVICE_ID)
+
+    alice_identity_key_pair = identity_key.IdentityKeyPair.generate()
+    bob_identity_key_pair = identity_key.IdentityKeyPair.generate()
+
+    alice_registration_id = 1 #TODO: generate these
+    bob_registration_id = 2
+
+    alice_store = storage.InMemSignalProtocolStore(alice_identity_key_pair, alice_registration_id)
+    bob_store = storage.InMemSignalProtocolStore(bob_identity_key_pair, bob_registration_id)
+
+    bob_pre_key_pair = curve.KeyPair.generate()
+    bob_signed_pre_key_pair = curve.KeyPair.generate()
+
+    bob_signed_pre_key_public = bob_signed_pre_key_pair.public_key().serialize()
+
+    bob_signed_pre_key_signature = bob_store.get_identity_key_pair() \
+                                            .private_key() \
+                                            .calculate_signature(bob_signed_pre_key_public)
+
+    pre_key_id = 31337
+    signed_pre_key_id = 22
+
+    bob_pre_key_bundle = state.PreKeyBundle(
+        bob_store.get_local_registration_id(),
+        DEVICE_ID,
+        pre_key_id,
+        bob_pre_key_pair.public_key(),
+        signed_pre_key_id,
+        bob_signed_pre_key_pair.public_key(),
+        bob_signed_pre_key_signature,
+        bob_store.get_identity_key_pair().identity_key(),
+    )
+
+    session.process_prekey_bundle(
+        bob_address,
+        alice_store,
+        bob_pre_key_bundle,
+    )
+
+    bob_prekey = state.PreKeyRecord(pre_key_id, bob_pre_key_pair)
+    bob_store.save_pre_key(pre_key_id, bob_prekey)
+
+    signed_prekey = state.SignedPreKeyRecord(
+            signed_pre_key_id,
+            42,
+            bob_signed_pre_key_pair,
+            bob_signed_pre_key_signature,
+        )
+    bob_store.save_signed_pre_key(signed_pre_key_id, signed_prekey)
+
+    assert alice_store.load_session(bob_address)
+    assert alice_store.load_session(bob_address).session_version() == 3
+
+    original_message = "Hobgoblins hold themselves to high standards of military honor"
+
+    assert bob_store.get_pre_key(pre_key_id)
+
+    outgoing_message = session_cipher.message_encrypt(alice_store, bob_address, original_message)
+    outgoing_message.message_type() == 3  # 3 == CiphertextMessageType::PreKey
+    outgoing_message_wire = outgoing_message.serialize()
+
+    edit_point = len(outgoing_message_wire) - 10
+    corrupted_message = outgoing_message_wire[:edit_point] + \
+                        bytes([outgoing_message_wire[edit_point] ^ 0x01]) + \
+                        outgoing_message_wire[edit_point + 1:]
+
+    incoming_message = protocol.PreKeySignalMessage.try_from(corrupted_message)
+
+    # This incoming message is corrupted, so we expect an exception to be raised
+    with pytest.raises(error.SignalProtocolError):
+        session_cipher.message_decrypt(bob_store, alice_address, incoming_message)
+
+    assert bob_store.get_pre_key(pre_key_id)
+
+    incoming_message = protocol.PreKeySignalMessage.try_from(outgoing_message_wire)
+
+    plaintext = session_cipher.message_decrypt(bob_store, alice_address, incoming_message)
+
+    assert original_message == plaintext.decode('utf8')
+
+    # Trying to get the prekey will now fail, as the prekey has been used and removed from the store
+    # TODO: Check this is SignalProtocolError::InvalidPreKeyId in the assert
+    with pytest.raises(error.SignalProtocolError):
+        assert bob_store.get_pre_key(pre_key_id)
