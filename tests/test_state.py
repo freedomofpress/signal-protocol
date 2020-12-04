f7fdbbab01a53870611f7f4f9677d1665146b40a
pierwill <pierwill@users.noreply.github.com>
2020-12-04 15:49:36 -0800

    Update tests for state.generate_n_prekeys()

--- tests/test_state.py
+++ tests/test_state.py
@@ -45,5 +45,7 @@ def test_define_prekey_bundle_under_prekey_exhaustion():
         bob_store.get_identity_key_pair().identity_key(),
     )
 
-    prekeyrecords = state.generate_n_prekeys(256, pre_key_id, bob_signed_pre_key_pair)
+    prekeyrecords = state.generate_n_prekeys(256, pre_key_id)
     assert len(prekeyrecords) == 256
+    assert prekeyrecords[12].id() != prekeyrecords[13].id()
+    assert prekeyrecords[14].key_pair() != prekeyrecords[15].key_pair()
