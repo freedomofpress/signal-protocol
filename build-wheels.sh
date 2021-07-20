2f24f6152bdef32e97dac7f9944b832483468bc8
redshiftzero <jen@redshiftzero.com>
2021-07-20 18:42:07 -0400

    build: update script for building wheels
    * fix path to rust-toolchain file
    * remove Python 3.5 support (not available in the manylinux2014
    container anyway)
    * remove build artifacts between builds

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ex
 
-RUST_TOOLCHAIN=$(cat rust-toolchain)
+RUST_TOOLCHAIN=$(cat /io/rust-toolchain)
 # We specify a particular rustup version and a SHA256 hash for
 # `rustup-init.sh`, computed ourselves and hardcoded here.
 RUSTUP_LATEST_VERSION=1.24.3
 OUR_RUSTUP_INIT_SHA="a3cb081f88a6789d104518b30d4aa410009cd08c3822a1226991d6cf0442a0f8"
 
-curl --proto '=https' --tlsv1.2 -sSf -O \
+curl --proto '=https' --tlsv1.3 -sSf -O \
   https://raw.githubusercontent.com/rust-lang/rustup/${RUSTUP_LATEST_VERSION}/rustup-init.sh
 # Verify checksum of rustup script.
 echo "${OUR_RUSTUP_INIT_SHA} rustup-init.sh" | sha256sum --check -
@@ -15,10 +15,10 @@ echo "${OUR_RUSTUP_INIT_SHA} rustup-init.sh" | sha256sum --check -
 sh rustup-init.sh --default-toolchain ${RUST_TOOLCHAIN} -y
 export PATH="${HOME}/.cargo/bin:${PATH}"
 
-
 cd /io
 
-for PYBIN in /opt/python/cp{35,36,37,38,39}*/bin; do
+for PYBIN in /opt/python/cp{36,37,38,39}*/bin; do
+    rm -f /io/build/lib.*
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
     "${PYBIN}/python" setup.py bdist_wheel
 done
